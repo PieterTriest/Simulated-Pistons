4f3d3091d7927bcf85d3b0bbe66da5621f6456b3
Pieter <pieter.triest@hotmail.com>
2026-05-01 15:52:43 +0200

    shaft on assembly

--- src/main/java/dev/pieter/simulated_pistons/client/SimulatedPistonLinkRenderer.java
+++ src/main/java/dev/pieter/simulated_pistons/client/SimulatedPistonLinkRenderer.java
@@ -2,22 +2,23 @@ package dev.pieter.simulated_pistons.client;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
+import com.simibubi.create.content.kinetics.base.KineticBlockEntityRenderer;
 import dev.pieter.simulated_pistons.content.SimulatedPistonLinkBlock;
 import dev.pieter.simulated_pistons.content.SimulatedPistonLinkBlockEntity;
 import net.createmod.catnip.render.CachedBuffers;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
-import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
 import net.minecraft.core.Direction;
 import net.minecraft.world.level.block.state.BlockState;
 
-public class SimulatedPistonLinkRenderer implements BlockEntityRenderer<SimulatedPistonLinkBlockEntity> {
+public class SimulatedPistonLinkRenderer extends KineticBlockEntityRenderer<SimulatedPistonLinkBlockEntity> {
     public SimulatedPistonLinkRenderer(final BlockEntityRendererProvider.Context context) {
+        super(context);
     }
 
     @Override
-    public void render(final SimulatedPistonLinkBlockEntity be, final float partialTick, final PoseStack ms, final MultiBufferSource buffer, final int packedLight, final int packedOverlay) {
+    protected void renderSafe(final SimulatedPistonLinkBlockEntity be, final float partialTick, final PoseStack ms, final MultiBufferSource buffer, final int packedLight, final int packedOverlay) {
         final BlockState state = be.getBlockState();
         if (!(state.getBlock() instanceof SimulatedPistonLinkBlock)) {
             return;
@@ -26,6 +27,8 @@ public class SimulatedPistonLinkRenderer implements BlockEntityRenderer<Simulate
         final Direction facing = state.getValue(SimulatedPistonLinkBlock.FACING);
         final Direction shaftDirection = facing.getOpposite();
         final VertexConsumer vb = buffer.getBuffer(RenderType.solid());
+
+        renderRotatingBuffer(be, CachedBuffers.partialFacing(SPPartialModels.SHAFT_SIXTEENTH, state, facing), ms, vb, packedLight);
 
         for (int segmentIndex = 0; segmentIndex < be.getChainLength(); segmentIndex++) {
             final int offset = segmentIndex;
